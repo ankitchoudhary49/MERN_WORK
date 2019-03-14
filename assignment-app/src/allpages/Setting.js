df89d8f2ebaaecd880a3e5a2f81b580a3a0c1e78
ankitchoudhary49 <ankitchoudhary49@gmail.com>
2019-03-14 11:27:23 +0530

    assignment

--- assignment-app/src/allpages/Setting.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Settings = () => {
-    return (
-        <div className='container center'>
-            <h2>This is Urvashi Singla</h2>
-        </div>
-    )
-}
-export default Settings;
